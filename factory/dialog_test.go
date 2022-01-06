18b46b5b3f41d768e81e1d14fa6c1899c7bef1f4
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-01-06 17:34:30 +0700

    add new unit test case

--- factory/dialog_test.go
+++ factory/dialog_test.go
@@ -3,14 +3,51 @@ package factory_test
 import (
 	"testing"
 
+	"github.com/adryanev/go-design-pattern/factory"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFactory(t *testing.T) {
+	dialogCreator := &factory.DialogCreator{}
+	t.Run("Test Dialog Type is HTML", func(t *testing.T) {
+		// setup
 
-	t.Run("Testing Assert", func(t *testing.T) {
-		// assert equality
-		assert.Equal(t, 123, 123, "they should be equal")
+		//do
+		htmlDialog, _ := dialogCreator.Render("html")
+		//assert
+		assert.IsType(t, &factory.HtmlDialog{}, htmlDialog)
+	})
+
+	t.Run("Test Dialog Type is Window", func(t *testing.T) {
+		//setup
+
+		//do
+		windowDialog, _ := dialogCreator.Render("windows")
+
+		//assert
+		assert.IsType(t, &factory.WindowsDialog{}, windowDialog)
+	})
+
+	t.Run("Test Window Dialog can render a button", func(t *testing.T) {
+
+		//setup
+		windowDialog, _ := dialogCreator.Render("windows")
+
+		//do
+		windowButton := windowDialog.CreateButton()
+		//assert
+		assert.IsType(t, &factory.WindowsButton{}, windowButton)
+	})
+
+	t.Run("Test Html Dialog can render a button", func(t *testing.T) {
+
+		//setup
+		htmlDialog, _ := dialogCreator.Render("html")
+
+		//do
+		htmlButton := htmlDialog.CreateButton()
+		//assert
+		assert.IsType(t, &factory.HtmlButton{}, htmlButton)
 	})
 
 }
